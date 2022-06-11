6f305fdac74d27922d4d2fcfc3d8d5a288443d31
raymondmills <millsraymondj@gmail.com>
2022-06-11 14:17:04 -0400

    Completed base.html, need to complete correspondong base.css

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+common = {
+    'first_name': 'Raymond',
+    'last_name': 'Mills',
+    'alias': ''
+}
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
